9f079df48db4fbc3609102e3890db17084b201ef
Shawood1028 <shawood1028@gmail.com>
2023-03-16 00:31:07 +0800

    netty discard example

--- demo/src/main/java/com/shawood/netty/discard/DiscardServer.java
+++ demo/src/main/java/com/shawood/netty/discard/DiscardServer.java
@@ -1,52 +1,47 @@
 package com.shawood.netty.discard;
 
+import com.shawood.netty.util.ServerUtil;
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.EventLoopGroup;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.ssl.SslContext;
+
 
 public class DiscardServer {
-    private int port;
+    static final int PORT = Integer.parseInt(System.getProperty("port", "8009"));
 
-    public DiscardServer(int port) {
-        this.port = port;
-    }
+    public static void main(String[] args) throws Exception {
+        //Configure SSL.
+        final SslContext sslCtx = ServerUtil.buildSslContext();
 
-    public void run() throws Exception {
         EventLoopGroup bossGroup = new NioEventLoopGroup(1);
         EventLoopGroup workerGroup = new NioEventLoopGroup();
         try {
             ServerBootstrap b = new ServerBootstrap();
-            b.group(bossGroup,workerGroup)
+            b.group(bossGroup, workerGroup)
                     .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
-                        public void initChannel(SocketChannel ch) throws Exception {
-                            ch.pipeline().addLast(new DiscardServerHandler());
+                        public void initChannel(SocketChannel ch) {
+                            ChannelPipeline p = ch.pipeline();
+                            if (sslCtx != null) {
+                                p.addLast(sslCtx.newHandler(ch.alloc()));
+                            }
+                            p.addLast(new DiscardClientHandler());
                         }
-                    })
-                    .option(ChannelOption.SO_BACKLOG, 128)
-                    .childOption(ChannelOption.SO_KEEPALIVE, true);
-            // bind and start to accept incoming connections.
-            ChannelFuture f = b.bind(port).sync();
+                    });
+
+            ChannelFuture f = b.bind(PORT).sync();
 
             f.channel().closeFuture().sync();
         } finally {
-            bossGroup.shutdownGracefully();
             workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
         }
-    }
-
-    public static void main(String[] args) throws Exception {
-        int port = 8080;
-        if (args.length > 0) {
-            port = Integer.parseInt(args[0]);
-        }
-
-        new DiscardServer(port).run();
     }
 }
---
+++ demo/src/main/java/com/shawood/netty/userguide/discard/DiscardServerHandler.java
@@ -0,0 +1,29 @@
+package com.shawood.netty.userguide.discard;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf in = (ByteBuf) msg;
+        try {
+            System.out.println("收到的信息为: ");
+            while (in.isReadable()) {
+                System.out.print(in.readByte());
+                System.out.flush();
+            }
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
